b7a036fc6c00ce88a75d5b215b4d5746b97fbdae
agomez-sf <abraham.gomez@salesforce.com>
2023-12-11 23:26:14 +0000

    fix: change snapshot command status to beta from pilot

--- src/commands/org/get/snapshot.ts
+++ src/commands/org/get/snapshot.ts
@@ -25,7 +25,7 @@ export class SnapshotGet extends SfCommand<OrgSnapshot> {
   public static readonly examples = messages.getMessages('examples');
   public static readonly aliases = ['force:org:snapshot:get'];
   public static readonly deprecateAliases = true;
-  public static readonly state = 'closedPilot';
+  public static readonly state = 'beta';
 
   public static readonly flags = {
     'target-dev-hub': requiredHubFlagWithDeprecations,
