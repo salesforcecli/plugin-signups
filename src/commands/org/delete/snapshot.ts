13e49a5e8d4d47ec253c91846738ab1da10b0071
JeffAMasloSF <82606972+JeffAMasloSF@users.noreply.github.com>
2024-07-11 09:18:28 -0600

    Removed unused definition from snapshot.ts

--- src/commands/org/delete/snapshot.ts
+++ src/commands/org/delete/snapshot.ts
@@ -48,7 +48,6 @@ export class SnapshotDelete extends SfCommand<SaveResult | undefined> {
 
   public async run(): Promise<SaveResult | undefined> {
     const { flags } = await this.parse(SnapshotDelete);
-    const snapshot = flags['snapshot'];
     if (!flags['no-prompt'] && !(await this.confirm({ message: messages.getMessage('prompt.confirm') }))) {
       return;
     }
